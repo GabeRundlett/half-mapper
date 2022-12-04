4d01a13219672d0b389c149a1c52f43db0000f80
GabeRundlett <gabe@rundlett.com>
2022-12-03 20:09:12 -0700

    initial commit

---
+++ src/entities.h
@@ -0,0 +1,8 @@
+#ifndef ENTITIES_H
+#define ENTITIES_H
+
+struct MapEntry;
+
+void parseEntities(const string &str, const string &id, const MapEntry &sMapEntry);
+
+#endif
---
+++ src/wad.h
@@ -0,0 +1,26 @@
+#ifndef WAD_H
+#define WAD_H
+
+#include <vector>
+
+// Extracted from http://hlbsp.sourceforge.net/index.php?content=waddef
+
+struct WADHEADER {
+    char szMagic[4];    // should be WAD2/WAD3
+    int32_t nDir;       // number of directory entries
+    int32_t nDirOffset; // offset into directory
+};
+
+struct WADDIRENTRY {
+    int32_t nFilePos;  // offset in WAD
+    int32_t nDiskSize; // size in file
+    int32_t nSize;     // uncompressed size
+    int8_t nType;      // type of entry
+    bool bCompression; // 0 if none
+    int16_t nDummy;    // not used
+    char szName[16];   // must be null terminated
+};
+
+int wadLoad(const std::vector<std::string> &szGamePaths, const string &filename);
+
+#endif
